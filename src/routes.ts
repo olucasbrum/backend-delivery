0e0f67d450a525971d374542a232595f0da354e7
Lucas Brum <olucasbrum@gmail.com>
2022-12-27 23:15:00 -0300

    feat(delivery): register delivery :sparkles:

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/authenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/createDeliverymanController';
 
 const routes = Router();
@@ -11,10 +12,14 @@ const createDeliverymanController = new CreateDeliverymanController();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
+const deliveryController = new CreateDeliveryController();
+
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client/', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+
+routes.post('/delivery', deliveryController.handle);
 
 export { routes };
