2e136b1cf049870e68f9c5e8c895f5bad352f1eb
Lucas Brum <olucasbrum@gmail.com>
2022-12-27 22:12:22 -0300

    feat: authenticate deliveryman :sparkles:

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/authenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/createDeliverymanController';
-CreateDeliverymanController;
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
-const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
-routes.post('/authenticate', authenticateClientController.handle);
+routes.post('/client/authenticate', authenticateClientController.handle);
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client/', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
